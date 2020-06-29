c01cdf2b5198f8b07c5c37f227ef032b49e03897
khrist14n <khrist14n@gmail.com>
2020-06-29 00:56:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-detcfj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-detcfj)
---
+++ index.ts
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+console.log("App Inicio")
+
+console.log("App Fin")
